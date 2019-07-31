0d4355129f13e0aee39e965d429048ea96d3f791
José Luis Landabaso Díaz <lbo@troy.local>
2019-07-31 18:06:57 +0200

    Rename files so that we can add more Blockchain types

--- runHonestNodesOneMessagePerBlock.bash
+++ runHonestNodesOneMessagePerBlock.bash
@@ -8,15 +8,15 @@ onINT() {
   exit
 }
 
-HTTP_PORT=20001 P2P_PORT=10001 npm run start &
+HTTP_PORT=20001 P2P_PORT=10001 npm run honestNodesOneMessagePerBlock &
 command1PID="$!" #grab the pid
 
 sleep 0.5
-HTTP_PORT=20002 P2P_PORT=10002 REMOTE_PEERS=localhost:10001 npm run start &
+HTTP_PORT=20002 P2P_PORT=10002 REMOTE_PEERS=localhost:10001 npm run honestNodesOneMessagePerBlock &
 command2PID="$!" #grab the pid
 
 sleep 0.5
-HTTP_PORT=20003 P2P_PORT=10003 REMOTE_PEERS=localhost:10001 npm run start &
+HTTP_PORT=20003 P2P_PORT=10003 REMOTE_PEERS=localhost:10001 npm run honestNodesOneMessagePerBlock &
 command3PID="$!" #grab the pid
 
 sleep 2
